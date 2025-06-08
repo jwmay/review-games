92a6e5e26b2442527adb84c1d589ecef09382737
Joseph May <jwmay87@gmail.com>
2025-06-07 23:11:22 -0700

    remove use-text-fit library (dns react v19)

--- app/components/Jeopardy/JeopardyCategoryTile.jsx
+++ app/components/Jeopardy/JeopardyCategoryTile.jsx
@@ -1,15 +1,7 @@
-import useTextFit from 'use-text-fit'
-
 export default function JeopardyCategoryTile({ category }) {
-  const { fontSize, ref } = useTextFit()
-
   return (
     <div className='grid bg-jeopardy-blue uppercase mb-4'>
-      <div
-        className='place-self-center text-4xl text-shadow-jeopardy-board tracking-wider'
-        //   ref={ref}
-        //   style={{ fontSize, height: 100, width: 100 }}
-      >
+      <div className='place-self-center text-4xl text-shadow-jeopardy-board tracking-wider'>
         {category}
       </div>
     </div>
