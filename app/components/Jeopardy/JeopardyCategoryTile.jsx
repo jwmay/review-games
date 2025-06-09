906f1a01fcd5733900d55108b6725ee4b1e4d39a
Joseph May <jwmay87@gmail.com>
2025-06-08 20:27:55 -0700

    try again with use-fit-text

--- app/components/Jeopardy/JeopardyCategoryTile.jsx
+++ app/components/Jeopardy/JeopardyCategoryTile.jsx
@@ -1,7 +1,15 @@
+import useFitText from 'use-fit-text'
+
 export default function JeopardyCategoryTile({ category }) {
+  const { fontSize, ref } = useFitText()
+
   return (
     <div className='grid bg-jeopardy-blue uppercase mb-4'>
-      <div className='place-self-center text-4xl text-shadow-jeopardy-board tracking-wider'>
+      <div
+        className='place-self-center text-4xl text-shadow-jeopardy-board tracking-wider'
+        ref={ref}
+        style={{ fontSize }}
+      >
         {category}
       </div>
     </div>
