bdaa010da3191c577659b790a83c72249e39dddd
Joseph May <jwmay87@gmail.com>
2025-06-12 11:42:47 -0700

    add zoom in animation for category names

--- app/components/Jeopardy/JeopardyBoard.jsx
+++ app/components/Jeopardy/JeopardyBoard.jsx
@@ -22,7 +22,11 @@ export default function JeopardyBoard({ data, onClick }) {
   return (
     <div className='animate-zoom-in bg-black h-screen grid grid-cols-[repeat(6,calc(95vw/6))] grid-rows-[repeat(6,calc(95vh/6))] gap-x-[1vw] gap-y-[1vh]'>
       {[...Array(6)].map((_, index) => (
-        <JeopardyCategoryTile category={data[index].category} key={index} />
+        <JeopardyCategoryTile
+          category={data[index].category}
+          id={index}
+          key={index}
+        />
       ))}
       {data.map((item) => (
         <JeopardyQuestionTile
--- app/components/Jeopardy/JeopardyCategoryTile.jsx
+++ app/components/Jeopardy/JeopardyCategoryTile.jsx
@@ -1,9 +1,15 @@
 import { AutoTextSize } from 'auto-text-size'
 
-export default function JeopardyCategoryTile({ category }) {
+export default function JeopardyCategoryTile({ category, id }) {
   return (
     <div className='jeopardy-board-tile mb-[1vh]'>
-      <AutoTextSize as='h2' maxFontSizePx={500} mode='box'>
+      <AutoTextSize
+        as='h2'
+        className='animate-zoom-in'
+        maxFontSizePx={500}
+        mode='box'
+        style={{ animationDelay: `${id + 3}s` }}
+      >
         {category}
       </AutoTextSize>
     </div>
