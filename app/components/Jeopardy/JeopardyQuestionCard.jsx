e9d82acb16ab21ac48530abbfd6eddd39db86111
Joseph May <jwmay87@gmail.com>
2025-06-12 17:20:28 -0700

    use enum for views

--- app/components/Jeopardy/JeopardyQuestionCard.jsx
+++ app/components/Jeopardy/JeopardyQuestionCard.jsx
@@ -5,16 +5,21 @@ import { useContextDispatch, useContextState } from './JeopardyContext'
 
 import { JEOPARDY_SET_SELECTED } from '../../actionTypes'
 
+const VIEWS = {
+  ANSWER: 'ANSWER',
+  QUESTION: 'QUESTION',
+}
+
 export default function JeopardyQuestionCard({ item }) {
-  const [display, setDisplay] = useState('question')
+  const [view, setView] = useState(VIEWS.QUESTION)
 
   const state = useContextState()
   const dispatch = useContextDispatch()
 
   function handleClick() {
-    const newDisplay = display === 'question' ? 'answer' : null
-    setDisplay(newDisplay)
-    if (!newDisplay) {
+    const newView = view === VIEWS.QUESTION ? VIEWS.ANSWER : null
+    setView(newView)
+    if (!newView) {
       dispatch({ type: JEOPARDY_SET_SELECTED, payload: { selected: null } })
     }
   }
@@ -24,11 +29,11 @@ export default function JeopardyQuestionCard({ item }) {
       className='jeopardy-card'
       onClick={handleClick}
       style={{
-        color: display === 'answer' ? 'var(--color-jeopardy-gold)' : '',
+        color: view === VIEWS.ANSWER ? 'var(--color-jeopardy-gold)' : '',
       }}
     >
       <AutoTextSize as='p' maxFontSizePx={96} minFontSizePx={24} mode='box'>
-        {display === 'question' ? parse(item.question) : parse(item.answer)}
+        {view === VIEWS.QUESTION ? parse(item.question) : parse(item.answer)}
       </AutoTextSize>
       {state.settings.showAmount && (
         <div className='absolute right-0 bottom-0 m-4 text-6xl'>
